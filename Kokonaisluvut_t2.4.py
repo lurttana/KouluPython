95cf08c5cc4176320136c2531e40d4c93741b3a3
lurttana <102549399+lurttana@users.noreply.github.com>
2025-01-16 12:58:29 +0200

    kokonaisluvut
    t 2.4 tehty

--- Kokonaisluvut_t2.4.py
+++ Kokonaisluvut_t2.4.py
@@ -3,4 +3,6 @@ for i in range(3):
     numbero= int(input("Anna kokonaisluku: "))
     lista.insert(i, numbero)
 summa= lista[0] + lista[1] + lista[2]
-print(summa)
+tulo= lista[0] * lista[1] * lista[2]
+keskiarvo= summa / len(lista)
+print(f"Summa on: {summa}, tulo on: {tulo} ja keskiarvo {keskiarvo}")
