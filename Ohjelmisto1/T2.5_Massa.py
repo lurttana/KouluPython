c79cc383888ac947e99e17ff67bb920604a0ede4
lurttana <102549399+lurttana@users.noreply.github.com>
2025-01-17 09:53:39 +0200

    Organization

---
+++ Ohjelmisto1/T2.5_Massa.py
@@ -0,0 +1,5 @@
+﻿leiviska=int(input("Montako leiviskää?: "))
+naula=int(input("Montako naulaa?: "))
+luoti=int(input("Montako luotia?: "))
+luotitotal= leiviska*20*32+naula*32+luoti
+massa= luotitotal*13,3
