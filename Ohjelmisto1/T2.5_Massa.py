98cfa81eff50845d3a9131ab32c05b2b5590a9f4
lurttana <102549399+lurttana@users.noreply.github.com>
2025-01-17 11:11:24 +0200

    Done

--- Ohjelmisto1/T2.5_Massa.py
+++ Ohjelmisto1/T2.5_Massa.py
@@ -1,5 +1,6 @@
-﻿leiviska=int(input("Montako leiviskää?: "))
-naula=int(input("Montako naulaa?: "))
-luoti=int(input("Montako luotia?: "))
-luotitotal= leiviska*20*32+naula*32+luoti
-massa= luotitotal*13,3
+﻿leiviska=float(input("Montako leiviskää?: "))
+naula=float(input("Montako naulaa?: "))
+luoti=float(input("Montako luotia?: "))
+total=(leiviska*20*32)+(naula*32)+luoti
+massa=total*13.3
+print(f"{int(massa/1000)} kilogrammaa ja {round(massa%1000, 2)} grammaa")
---
+++ Ohjelmisto1/T2.6_Arpa.py
@@ -0,0 +1,9 @@
+﻿import random
+kolmenum=[]
+nelinum= []
+for i in range(4):
+    if i <3:
+        kolmenum.append(random.randint(0,9))
+    nelinum.append(random.randint(1,6))
+print(*kolmenum)
+print(*nelinum)
