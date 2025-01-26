5f388f7805af4e48af45226744eabe711f480f5c
lurttana <102549399+lurttana@users.noreply.github.com>
2025-01-26 17:51:01 +0200

    t4.1-3 done

---
+++ Ohjelmisto1/T4.2.py
@@ -0,0 +1,5 @@
+﻿while True:
+    tuumat=float(input("Anna tuumat:"))
+    if tuumat<0:
+        break
+    print(tuumat*2.54)
---
+++ Ohjelmisto1/T4.3.py
@@ -0,0 +1,9 @@
+﻿luvut=[]
+while True:
+    luku=input("Syötä luku: ")
+    if luku=="":
+        break
+    if luku.isnumeric():
+        luvut.append(luku)
+if len(luvut)>0:
+    print(f"Pienin luku on {min(*luvut)} ja suurin luku on {max(*luvut)}")
