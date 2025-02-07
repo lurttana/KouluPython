c801b00d73b057666971c9be87a15bb61e67c224
lurttana <102549399+lurttana@users.noreply.github.com>
2025-02-07 09:47:16 +0200

    comment

--- Ohjelmisto1/T6.4.py
+++ Ohjelmisto1/T6.4.py
@@ -3,6 +3,7 @@
     for i in range(len(luku_lista)):
         lukusumma+=luku_lista[i]
     return lukusumma
+    # for luku in lukulista:
 
 kokonaislukuja=[2,3,6,8]
 print(lukusummaaja(kokonaislukuja))
