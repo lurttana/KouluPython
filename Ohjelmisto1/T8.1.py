cb72f6f53a03bf282c5a72768c21bbc749c6d2bf
lurttana <102549399+lurttana@users.noreply.github.com>
2025-02-13 09:34:10 +0200

    add municipality

--- Ohjelmisto1/T8.1.py
+++ Ohjelmisto1/T8.1.py
@@ -15,11 +15,11 @@ ICAO-koodi on tallennettuna airport-taulun ident-sarakkeeseen."""
 
 def icao_haku():
     icao=input("Anna icao koodi: ")
-    sql=f"SELECT name FROM airport WHERE ident = '{icao}'"
+    sql=f"SELECT name,municipality  FROM airport WHERE ident = '{icao}'"
     kursori=yhteys.cursor()
     kursori.execute(sql)
     tulos= kursori.fetchall()
     for nimi in tulos:
-        print(nimi)
+        print(f"{nimi[0]} , {nimi[1]}")
 
 icao_haku()
