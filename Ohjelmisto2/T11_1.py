eec8d295d48d4419b0b829337eee4e6b07dcedc5
lurttana <102549399+lurttana@users.noreply.github.com>
2025-03-21 15:13:15 +0200

    moduuli 11 t 1 done

---
+++ Ohjelmisto2/T11_1.py
@@ -0,0 +1,30 @@
+﻿"""Toteuta seuraava luokkahierarkia Python-kielellä: Julkaisu voi olla kirja tai lehti.
+Jokaisella julkaisulla on nimi. Kirjalla on lisäksi kirjoittaja ja sivumäärä, kun taas lehdellä on päätoimittaja.
+Kirjoita luokkiin myös tarvittavat alustajat. Tee aliluokkiin metodi tulosta_tiedot, joka tulostaa kyseisen julkaisun kaikki tiedot.
+Luo pääohjelmassa julkaisut Aku Ankka (päätoimittaja Aki Hyyppä) ja Hytti n:o 6 (kirjailija Rosa Liksom, 200 sivua).
+Tulosta molempien julkaisujen kaikki tiedot toteuttamiesi metodien avulla."""
+
+class Julkaisu:
+    def __init__(self, nimi):
+        self.nimi=nimi
+
+class Kirja(Julkaisu):
+    def __init__(self,nimi, kirjoittaja, sivut):
+        super().__init__(nimi)
+        self.kirjoittaja=kirjoittaja
+        self.sivut=sivut
+    def tulosta_tiedot(self):
+        print(f"{self.nimi} {self.kirjoittaja} {self.sivut}")
+
+class Lehti(Julkaisu):
+    def __init__(self, nimi, paatoimittaja):
+        super().__init__(nimi)
+        self.paatoimittaja=paatoimittaja
+    def tulosta_tiedot(self):
+        print(f"{self.nimi} {self.paatoimittaja}")
+
+aku= Lehti("Aku Ankka", "Aki Hyyppä")
+hytti= Kirja("Hytti n:o 6", "Rosa Liksom", 200)
+aku.tulosta_tiedot()
+hytti.tulosta_tiedot()
+
